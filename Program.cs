75d3508c4cd8d42d555b0b4cc6909728ed2282f7
inspiringhobbyist <inspiringhobbyist@gmail.com>
2020-08-21 10:43:35 -0600

    practice

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
 ﻿using System;
+namespace myapplication
+{
+    class program
+    {
+       static void mymethod(string country =  "norway");
+        {  
+        console.writeline(country);
+        
+        }
+
+       static void Main(string[] args)
+       {
+         mymethod("sweden");
+         mymethod("india");
+
+       }
+    }
+}
